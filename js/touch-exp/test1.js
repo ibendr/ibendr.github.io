7183bb0af2229e1a53d9fb360e97d0ae9ddaf468
Ben D Robinson <i.bendr@gmail.com>
2016-11-03 20:53:26 +1100

    Update test1.js

--- js/touch-exp/test1.js
+++ js/touch-exp/test1.js
@@ -5,8 +5,6 @@ it.style.height = window.screen.height - 48 ;
 var console = document.createElement( 'div' ) ;
 it.appendChild( console ) ;
 console.write = function( s ) {
-8
-  var txt =  ;
   console.appendChild( document.createTextNode( s ) ) ;
   console.appendChild( document.createElement( 'br' ) ) ;
 } ;
