39b14dd05da621021cc04eb1281ef8fbba4a3bed
ben <i.bendr@gmail.com>
2017-05-04 13:48:56 +1000

    Put animaton frame polyfill back in
    Used animframe_polyfill.js as is from old 96 code.
    Had just discovered code wasn't running in Iceweasel!

--- xwd/js/xwdMaster3.js
+++ xwd/js/xwdMaster3.js
@@ -39,7 +39,7 @@ var links = 	'<link href="' + cssMain + '" rel="stylesheet" type="text/css">\n' 
 		'<link rel="shortcut icon" type="image/x-icon" href="../favicon.ico">\n' +
 		'<script type="text/javascript" src="xwdLocal.js"></script>\n';
 document.write( links ) ;
-include(   "watcher" , "object2" , "xwd3" , "xwdInterface3" , jsInterface ) ;
+include(  "animframe_polyfill" , "watcher" , "object2" , "xwd3" , "xwdInterface3" , jsInterface ) ;
 
 // Wait till the browser is ready to render the game (avoids glitches)
 
