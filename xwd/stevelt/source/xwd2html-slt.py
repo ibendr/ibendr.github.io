4c516b84b6a515c2cff8417e726def84617b568c
ben <i.bendr@gmail.com>
2018-12-20 14:05:10 +1100

    Added Steve's source directory to repository

---
+++ xwd/stevelt/source/xwd2html-slt.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+"""
+Simple program to create html xwd files from 'draft'
+text xwd files - where each clue is in format:
+  <n>. <SOLUTION> (<LENGTH>) <CLUE>
+and of course we want it as:
+  <n>. <CLUE> (<LENGTH>)
+We also move the solution (grid) to the end,
+and add 3 intervening lines of html.
+WARNING: This is a crude program, e.g. demands
+exactly one blank line between lines of grid
+and clues, only translates clue format if the
+clue fits the test just right.
+"""
+
+import sys , subprocess , re
+import random
+
+# Global variables
+
+html1 = '<html><body><pre class="xwd">'
+html2 = '<![CDATA['
+html3 = 'Author:by Steve Townsend\n]]></pre></body>\n' + \
+        '<script type="text/javascript" src="../js/xwdMaster4.js">\n' + \
+        '</script></html>'
+
+if __name__ == "__main__":
+    import sys
+    for fname in list ( sys.argv[ 1 : ] ):
+	print fname
+	src = file( fname ).read( ).splitlines( )
+	grid, clues = [ ] , [ ]
+	i = 0
+	# Up to first blank line is grid
+	while i < len( src ) and src[ i ].strip( ):
+	    grid.append( src[ i ].upper( ) )
+	    i += 1
+	# Then clues
+	i += 1
+	while i < len( src ) and src[ i ].strip( ):
+	    l = src[ i ]
+	    # To translate, must have  . ( ) in that order
+	    if 0 < l.find( '.' ) < l.find( '(' ) < l.find( ')' ) < len( l ) - 2:
+		l = l[ : l.find( '.' ) + 1 ] + l[ l.find( ')' ) + 1 : ] + \
+		    l[ l.find( '(' ) - 1 : l.find( ')' ) + 1 ]
+            else:
+                # display unparsed lines amongst clues
+                print l
+	    clues.append( l )
+	    i += 1
+	# We'll add a name tag if it's in the puzzle<n> series...
+	if fname[ 3 : 6 ] == 'slt':
+	    grid.append( "Name: Puzzle " + fname[ : 3 ] )
+	out = [ html1 ] + clues + [ html2 ] + grid + [ html3 ]
+	# output
+	file( '/home/ben/programming/ibendr.github.io/xwd/stevelt/slt07-' + fname[ : 3 ] + '.html' , 'w' ).write( '\n'.join( out ) )
+
+	
+      
+      
