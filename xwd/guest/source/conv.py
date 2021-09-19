e9c2aca14b386eb62e802d192545b4e3a31fb73d
ben <i.bendr@gmail.com>
2021-09-20 08:27:07 +1000

    Steve 234 for Sep 20

--- xwd/guest/source/conv.py
+++ xwd/guest/source/conv.py
@@ -68,6 +68,8 @@ if __name__ == "__main__":
 	out = [ html1 ] + clues + [ html2 ] + grid + [ html3 ]
 	# output
 	fout = fpath + fname + '.html'
+	if fnam[-4:]==".txt":
+		fout = fpath + fname[:-4] + '.html'
 	file( fout , 'w' ).write( '\n'.join( out ) )
 	os.system( 'git add ' + fname + ' ' + fout  )
 	#file( '../' + fname + '.html' , 'w' ).write( '\n'.join( out ) )
